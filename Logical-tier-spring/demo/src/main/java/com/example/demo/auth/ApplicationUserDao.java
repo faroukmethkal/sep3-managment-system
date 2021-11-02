02425eee99dbb51c4c618554bb94895f587cf505
faroukmethkal <faroukmethkal@gmail.com>
2021-11-02 19:54:40 +0100

    Delete Logical-tier-spring directory
    de

--- Logical-tier-spring/demo/src/main/java/com/example/demo/auth/ApplicationUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.auth;
-
-import org.springframework.security.core.userdetails.User;
-
-public interface ApplicationUserDao {
-
-     ApplicationUser selectApplicationUserByUsername(String username);
-}
