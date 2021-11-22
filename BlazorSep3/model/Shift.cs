df8228d2c596a5b3f3e3007930b682deb4234127
terezeRafajova <79629918+terezeRafajova@users.noreply.github.com>
2021-11-22 15:35:03 +0100

    shift

---
+++ BlazorSep3/model/Shift.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BlazorSep3.model
+{
+    public class Shift
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("name")] 
+        public string Name { get; set; }
+        [JsonProperty("date")] 
+        public  DateTime Date { get; set; }
+        [JsonProperty("startTime")] 
+        public DateTime StartTime { get; set; }
+        [JsonProperty("endTime")] 
+        public DateTime EndTime { get; set; }
+        [JsonProperty("description")] 
+        public string Description { get; set; }
+        [JsonProperty("numberOfEmployees")] 
+        public int NumberOfEmployee { get; set; }
+    }
+}
